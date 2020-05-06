ed10da754f2e436dc033258741fca912d24d937a
”Nicholas <nward9@uncc.edu>
2020-05-06 16:13:32 +0000

    Added testing for feature: Blogger can like an article

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature"Blogger creates a new user account and signs into it" do
+feature "Blogger creates a new user account and signs into it" do
     scenario "Blogger sucessfully creates a new user account" do
         visit signup_path
         expect(page).to have_content("New User")
@@ -31,7 +31,7 @@ feature"Blogger creates a new user account and signs into it" do
     end
 end
 
-feature"Blogger creates a new Article" do
+feature "Blogger creates a new Article" do
     scenario "Blogger signs into their account" do
         visit signup_path
         expect(page).to have_content("New User")
@@ -85,9 +85,68 @@ feature"Blogger creates a new Article" do
     end
 end
 
-feature"Another Feature here" do
-    scenario "Another Scenario here" do
+feature "Blogger has the ability to like an article" do
+    scenario "Blogger successfully creates a new article" do
+        # USER ACCOUNT CREATED AND SIGNED IN
         visit signup_path
         expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
+        visit login_path
+        expect(page).to have_content("Login")
+        fill_in "Email", with: "capybaraemail@gmail.com"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Featured Article") 
+        # USER IS NOW SIGNED IN
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Submit"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
     end
+    scenario "Blogger likes an article" do
+        # USER ACCOUNT CREATED AND SIGNED IN
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
+        visit login_path
+        expect(page).to have_content("Login")
+        fill_in "Email", with: "capybaraemail@gmail.com"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Featured Article") 
+        # USER IS NOW SIGNED IN
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Submit"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+        # USER NAVIGATES TO THE ARTICLE LISTING PAGE
+        visit articles_url
+        expect(page).to have_content("Article Listing")
+        click_text = "Show"
+        page.find(:xpath,"//*[text()='#{click_text}']").click
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+        expect(page).to have_content("Likes: 0")
+        click_text = "Like article"
+        page.find(:xpath,"//*[text()='#{click_text}']").click
+        expect(page).to have_content("Likes: 1")
+    end 
 end
