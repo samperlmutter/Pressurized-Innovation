c9a88d99f6776270c464c80e07111dac3ec3c3c7
”Nicholas <nward9@uncc.edu>
2020-05-06 18:37:26 +0000

    Added testing for feature: Blogger can edit an article

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -340,3 +340,72 @@ feature "Blogger can view articles on the article listing page" do
         expect(page).to have_content("This is another new Capybara article")
     end 
 end
+
+feature "Blogger can edit an article" do
+    scenario "Blogger successfully creates a new article" do
+        # USER ACCOUNT CREATED AND SIGNED IN
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
+        visit login_path
+        expect(page).to have_content("Login")
+        fill_in "Email", with: "capybaraemail@gmail.com"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Featured Article") 
+        # USER IS NOW SIGNED IN
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Submit"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+    end
+    scenario "Blogger edits an article" do
+        # USER ACCOUNT CREATED AND SIGNED IN
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
+        visit login_path
+        expect(page).to have_content("Login")
+        fill_in "Email", with: "capybaraemail@gmail.com"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Featured Article") 
+        # USER IS NOW SIGNED IN
+        # USER CREATES A NEW ARTICLE
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Submit"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+        # USER THEN EDITS THE ARTICLE
+        visit articles_url
+        expect(page).to have_content("Article Listing")
+        page.find(:xpath,"//*[text()='#{"Edit"}']").click
+        expect(page).to have_content("Editing article")
+        fill_in "Title", with: "Edited New Capybara Article"
+        fill_in "Text", with: "This is an edited new Capybara article"
+        click_button "Submit"
+        expect(page).to have_content("Edited New Capybara Article")
+        expect(page).to have_content("This is an edited new Capybara article")
+        visit articles_url
+        expect(page).to have_content("Edited New Capybara Article")
+        expect(page).to have_content("This is an edited new Capybara article")
+    end 
+end
