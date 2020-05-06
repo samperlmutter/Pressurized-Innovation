c7fdb8e985ae15043739351e18456d2b6627c8bf
”Nicholas <nward9@uncc.edu>
2020-05-06 15:29:30 +0000

    Added testing for feature: Blogger creates a new user account and signs into it

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -1,22 +1,33 @@
 require 'rails_helper.rb'
 
-feature"Blogger adds an article" do
-    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
-        visit articles_path
-        expect(page).to have_content("Listing articles")
-        click_link "New article"
-        expect(page).to have_content("New Article")
-        expect(page).to have_field("Title")
-        expect(page).to have_field("Text")
+feature"Blogger creates a new user account and signs into it" do
+    scenario "Blogger sucessfully creates a new user account" do
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
     end
-    
-    scenario "Blogger successfully creates a new article" do
-        visit new_article_path
-        expect(page).to have_content("New Article")
-        fill_in "Title", with: "New Capybara Article"
-        fill_in "Text", with: "This is a new Capybara article"
-        click_button "Create Article"
-        expect(page).to have_content("New Capybara Article")
-        expect(page).to have_content("This is a new Capybara article")
+    scenario "Blogger logs into their newly created user account" do
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Capybara Name"
+        fill_in "Email", with: "CapybaraEmail@gmail.com"
+        fill_in "Bio", with: "Capybara bio"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Create User"
+        expect(page).to have_content("Capybara Name")
+        visit login_path
+        expect(page).to have_content("Login")
+        fill_in "Email", with: "capybaraemail@gmail.com"
+        fill_in "Password", with: "123456"
+        click_button "Login"
+        expect(page).to have_content("Featured Article")  
     end
+
 end
